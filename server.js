080e00c9649f3cc4e9dd04cc722e6ef13cb79f83
Amy Lee <amyleeannebennett@gmail.com>
2016-02-02 13:43:04 -0600

    params are working correctly based on month and year

--- server.js
+++ server.js
@@ -1,36 +1,59 @@
 'use strict';
-const http = require('http');
+const express = require('express');
+//shorten it to require('express')();
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-http.createServer((req, res) => {
-  console.log(req.method, req.url);
 
-  if (req.url === '/hello') {
-    const msg = '<h1>Hello World</h1>';
+app.get('/hello', (req,res) => {
+  const name = req.query.name;
+  const msg = `<h1>Hello ${name}!</h1>`;
+ console.log('query params', req.query);
 
-    msg.split('').forEach((char, i) => {
-      setTimeout(() => {
-      res.write(char);
-    }, 1000 * i);
+  msg.split('').forEach((char, i) => {
+    setTimeout(() => {
+    res.write(char);
+  }, 1000 * i);
   });
 
-   setTimeout(() => {
-     res.end('end')
-   }, 20000);
-  } else if (req.url === '/random'){
-    res.end(Math.random().toString());
-  } else {
-    res.writeHead(403);
-    res.end('Access Denied');
-  }
-
-  //debugger;
+  setTimeout(() => {
+    res.end('end')
+  }, 20000);
 
   res.writeHead(200, {
-    'Content-type': 'text/html'
-  });
+   'Content-type': 'text/html'
+  })
+});
 
-}).listen(PORT, () => {
+app.get('/cal/:mon/:yr', (req, res) => {
+  const months = require('node-cal/lib/months');
+  console.log("months", months);
+  const mon = req.params.mon;
+  const yr = req.params.yr;
+  res.send(`<pre>${months.fullMonth(mon, yr)}</pre>`);
+});
+
+app.get('/random', (req, res) => {
+  res.send(Math.random().toString());
+});
+
+function getRandomInt (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+app.get('/random/:min/:max', (req, res) => {
+  const min = req.params.min;
+  const max = req.params.max;
+  res.send(getRandomInt(+min, +max).toString());
+});
+
+app.all('*', (req, res) => {
+  res
+  .status(403)
+  .send('Access Denied!');
+});
+
+app.listen(PORT, () => {
   console.log(`Node.js server started. Listening on port ${PORT}`);
-  // put into terminal -- PORT=1337 node --harmony_destructuring server.js
+// put into terminal -- PORT=1337 node --harmony_destructuring server.js
 });
