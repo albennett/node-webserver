cb2bf98767b5b922663f674c7b2894d5df863197
Amy Lee <amyleeannebennett@gmail.com>
2016-02-04 11:52:07 -0600

    jquery is working on splash page

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,9 +1,12 @@
-var changeImage = function() {
+
+function changeImage(){
+  console.log("hi");
   var image = document.getElementById('image');
   image.src = "../images/garden-party-animal-dog";
 }
 
 $(document).on('click', 'h1', function(){
+  console.log("hello");
   $(this).fadeOut("fast");
   $(this).fadeIn('slow');
 });
